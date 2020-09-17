405b4ef76df3c88616699ab76e0eac6dc7e76466
Ye Yuan <yeyuan@MacBook-Pro.local>
2020-09-17 17:16:58 -0400

    Use block data instead

--- acs_etl.py
+++ acs_etl.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import censusdata
-def get_tract_data_in_state(variables, state='36', county='*', tract='*', data_type='acs5', year=2018, table_type='profile'):
-    if tract:
-        data = censusdata.download(
-                data_type, year,
-                censusdata.censusgeo([('state', state), ('county', county), ('tract', tract)]),
-                list(variables.keys()),
-                tabletype=table_type)
+def get_data_in_state(variables, state='36', county='*', block='*', data_type='acs5', year=2018):
+    if block:
+        datas = []
+        geos = censusdata.geographies(censusdata.censusgeo([('state', state),('county', county)]), 'acs5', 2015)
+        for geo in list(geos.values())[:3]:
+            cur_data = censusdata.download(
+                    data_type, year,
+                    censusdata.censusgeo([('state', state), ('county', geo.params()[1][1]), ('block group', block)]),
+                    list(variables.keys()))
+            datas.append(cur_data)
+        data = pd.concat(datas)
     elif county:
         data = censusdata.download(
                 data_type, year,
@@ -22,33 +26,29 @@ def get_tract_data_in_state(variables, state='36', county='*', tract='*', data_t
     data.rename(columns=variables, inplace=True)
     return data
 
-variables = {'DP05_0070E': 'Estimate!!HISPANIC OR LATINO AND RACE!!Total population',
-             'DP05_0071E': 'Hispanic or Latino (any race)',
-             'DP05_0077E': 'White alone',
-             'DP05_0078E': 'Black or African American alone',
-             'DP05_0079E': 'American Indian and Alaska Native alone',
-             'DP05_0080E': 'Asian alone',
-             'DP05_0081E': 'Native Hawaiian and Other Pacific Islander alone',
-             'DP02_0001E': 'Estimate!!HOUSEHOLDS BY TYPE!!Total households',
-             'DP02_0002E': 'Estimate!!HOUSEHOLDS BY TYPE!!Total households!!Family households (families)',
-             'DP03_0062E': 'Estimate!!INCOME AND BENEFITS (IN 2018 INFLATION-ADJUSTED DOLLARS)!!Total households!!Median household income (dollars)',
-             'DP03_0063E': 'Estimate!!INCOME AND BENEFITS (IN 2018 INFLATION-ADJUSTED DOLLARS)!!Total households!!Mean household income (dollars)',
-             'DP03_0096E': 'Estimate!!HEALTH INSURANCE COVERAGE!!Civilian noninstitutionalized population!!With health insurance coverage',
-             'DP05_0018E': 'Estimate!!SEX AND AGE!!Total population!!Median age (years)',
-             'DP03_0032E': 'Estimate!!INDUSTRY!!Civilian employed population 16 years and over',
-             'DP03_0035E': 'Estimate!!INDUSTRY!!Civilian employed population 16 years and over!!Manufacturing',
+variables = {'B02001_001E': 'total_population',
+             'B03001_003E': 'hispanic_or_latino',
+             'B02001_002E': 'white',
+             'B02001_003E': 'black_or_African',
+             'B02001_004E': 'indian_and_alaska_native',
+             'B02001_005E': 'asian',
+             'B02001_006E': 'native_hawaiian_and_other_pacific_islander',
+             'B09019_001E': 'total_in_living_units',
+             'B09019_002E': 'total_in_households',
+             'B09019_003E': 'in_family_households',
+             'B06011_001E': 'median_income',
+             'B01002_001E': 'median_age',
+             'B23013_001E': 'aggregate_usual_work_hours',
+             'C24050_001E': 'total_employed',
+             'C24050_004E': 'manufacturing_employed',
+             'C24050_007E': 'transportation_warehousing_utilities_employed'
             }
+data = get_data_in_state(variables, state='36', county='*', block='*', data_type='acs5', year=2018)
 
-if __init__ == '__main__':
-    data = get_tract_data_in_state(
-                variables, state='36', county='*', tract='*', data_type='acs5', 
-                year=2018, table_type='profile'
-           )
-    
-    data['State'] = [data.index[i].params()[1][1] for i in range(data.shape[0])]
-    data['County'] = [data.index[i].params()[1][1] for i in range(data.shape[0])]
-    data['Tract'] = [data.index[i].params()[2][1] for i in range(data.shape[0])]
-    data.reset_index(drop=True, inplace=True)
-    
-    data.to_csv('acs_data_yeyuan2.csv')
-    
+data['State'] = [data.index[i].params()[0][1] for i in range(data.shape[0])]
+data['County'] = [data.index[i].params()[1][1] for i in range(data.shape[0])]
+data['Tract'] = [data.index[i].params()[2][1] for i in range(data.shape[0])]
+data['BlockGroup'] = [data.index[i].params()[3][1] for i in range(data.shape[0])]
+data.reset_index(drop=True, inplace=True)
+
+data.to_csv('acs_data_yeyuan2.csv')
